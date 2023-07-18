dcb17ef58130d50d17837a8202bfb0387fa00e18
taKana671 <kanae5321@gmail.com>
2023-07-18 23:29:39 +0900

    Remove unused code.

--- start_screen.py
+++ start_screen.py
@@ -1,6 +1,6 @@
 from panda3d.core import NodePath, TransparencyAttrib
 from panda3d.core import Shader
-from panda3d.core import BitMask32, Point3, CardMaker
+from panda3d.core import Point3, CardMaker
 
 
 class StartScreen:
@@ -8,7 +8,6 @@ class StartScreen:
     def __init__(self):
         self.alpha = 1.0
         self.create_color_gradient()
-        self.create_color_camera()
 
     def create_color_gradient(self):
         cm = CardMaker('gradient')
@@ -26,22 +25,11 @@ class StartScreen:
         self.color_plane.set_shader_input('u_resolution', props.get_size())
         self.color_plane.set_shader_input('alpha', self.alpha)
 
-    def create_color_camera(self):
-        self.color_buffer = base.win.make_texture_buffer('gradieng', 512, 512)
-        self.color_buffer.set_clear_color(base.win.get_clear_color())
-        self.color_buffer.set_sort(-1)
-
-        self.color_camera = base.make_camera(self.color_buffer)
-        self.color_camera.node().set_lens(base.camLens)
-        self.color_camera.node().set_camera_mask(BitMask32.bit(1))
-
     def set_up(self):
         self.color_plane.reparent_to(base.render)
-        self.color_camera.reparent_to(base.render)
 
     def tear_down(self):
         self.color_plane.detach_node()
-        self.color_camera.detach_node()
 
     def appear(self, dt):
         if self.alpha == 1.0:
